a8f61f20d7a24783aaab9e373f857ba9a51c2425
ChayanneClub <donnacho005@gmail.com>
2022-01-18 22:04:25 -0300

    Se agrega el proyecto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Portal Coyhaique</title>
+</head>
+
+<body>
+  <!--HEADER-->
+  <header>
+    <style>
+
+    </style>
+    <section class="imagen">
+
+      <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 h-screen">
+        <nav class="flex pb-8 items-center justify-between flex-wrap mr-10 mb-8 p-6">
+          <a href="#"><img src="images/imagenes1/logotipo.png" class="" alt=""></a>
+          <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight"></span>
+          </div>
+          <div class="block lg:hidden">
+            <button id='boton'
+              class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div id='menu' class="w-full block  lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+              <a href="#"
+                class="bg-yellow-400 rounded-full py-2 px-7 block mt-4 lg:inline-block lg:mt-0 text-teal-500 mr-4 ">
+                <b>INICIO</b>
+              </a>
+              <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 text-white mr-4">
+                NOSOTROS
+              </a>
+              <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 text-white mr-4">
+                MARCAS
+              </a>
+              <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 text-white mr-4">
+                GALERIA
+              </a>
+              <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 text-white mr-4">
+                CARRITO
+              </a>
+              <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-500 text-white mr-4">
+                ESCRIBENOS
+              </a>
+
+            </div>
+
+          </div>
+
+        </nav>
+        <div class="flex flex-row-reverse">
+          <img src="images/imagenes1/fonocaricatura.png" class="ml-auto mr-36" alt="">
+          <div clas="">
+            <img src="images/imagenes1/marketing.png" class="ml-52 mt-28" alt="">
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+    </section>
+
+
+  </header>
+  <!--HEADER-->
+
+
+
+  <!--Section articulo 1-->
+  <section class="imagen2 bg-no-repeat">
+    <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 h-screen">
+
+      <article>
+        <div class="flex justify-center mr-16">
+          <div class="text-center mt-48">
+            <h3 class="text-6xl"><b>DESARROLLO</b></h1>
+              <h1 class="text-7xl"><b>WEB &</b></h1>
+              <h1 class="text-7xl"><b>APLICACIONES</b></h1>
+              <p class="uppercase text-xl"> Creacion y Redaccion de protocolo y convenios</p>
+              <a href=""> <img src="images/imagenes2/botonConoce.png" class="ml-40 mt-10">
+          </div>
+        </div>
+      </article>
+  </section>
+
+  <!--Section articulo 2-->
+  <section class="imagen3">
+    <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 h-screen">
+      <article>
+        <div class="">
+          
+          <h2 class="text-5xl text-yellow-300 text-center"><B>FEED E HISTORIAS</B></h2>
+          <h3 class="text-5xl text-center"><B>ANIMACION & PIEZAS GRAFICAS PARA REDES SOCIALES</B></h3>
+        </div>
+        <div class="grid grid-cols-3 gap-2 mt-10">
+                  <!--FEED1-->
+
+      <img class="ml-20" src="images/imagenes3/1.png" alt="">
+      <img class="ml-10" src="images/imagenes3/2.png" alt="">
+      <img src="images/imagenes3/3.png" alt="">
+
+        </div>
+
+
+      </article>
+  </section>
+  <!--Section articulo 3-->
+
+  <section class="imagen4">
+    <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 py-10  h-screen">
+      <article>
+<div class="text-center uppercase ">
+<h1 class="text-yellow-400 text-3xl"><b>PROTOCOLO/SEGUIMIENTO</b></h1>
+<h1 class="text-white text-4xl"><b>EVALUACIONES Y PROCEDIMIENTOS</b></h1>
+</div>
+<!--DIV COLS ARRIBA-->
+<div class="grid grid-cols-2 mt-24 ml-32 ">
+  <!--DIV TABLA-->
+  <div class="text-left ml-20">
+<img src="images/imagenes4/tabla.png" class="float-left pb-28" alt="">
+<p class="text-yellow-300 uppercase text-xl ml-28"><b>BRIEFING</b></p>
+<p class="text-white ml-28">Recepcion de toda la información valida, actual y</p>
+<p class="text-white ml-28">coherente al grupo.</p>
+<p class="text-white ml-28 ">Ideas y apoyo visual para una optimizacion del</p>
+<p class="text-white ml-28">entendimiento para una guiada estrategia de</p>
+<p class="text-white  ml-28">interpretacion y posterior desarrollo de diseños</p>
+
+</div>
+<!--DIV DERECHA-->
+<div class="text-justify">
+<img src="images/imagenes4/relojdearena.png " class="float-left pb-10">
+<p class="text-yellow-300 uppercase  text-xl ml-28 "><b>EVALUACIONES Y MODIFICACIONES</b></p>
+<p class="text-white ml-28">Periodo en el cual el tiempo y acuerdos quedan sujetos</p>
+<p class="text-white ml-28">segun cantidad de modificaciones y mejoras del</p>
+<p class="text-white ml-28">producto, proyecto y/o servicio</p>
+</div>
+
+<!--DIV ABAJO IZQUIERDA-->
+<div class="text-justify mt-14 ml-20">
+  <img src="images/imagenes4/castillo.png " class="float-left ">
+  <p class="text-yellow-300 uppercase  text-xl ml-28"><b>DESARROLLO CREATIVO</b></p>
+  <p class="text-white ml-28">En un periodo de 5 dias habiles, se le hara llegar</p>
+  <p class="text-white ml-28">a correo y whatsapp, presentacion de Lluvia de </p>
+  <p class="text-white ml-28">ideas que se conformara por 3 imagenes y 3</p>
+  <p class="text-white ml-28">propuesta de cada una.</p>
+  </div>
+
+  <!--DIV ABAJO DERECHA-->
+<div class="text-justify mt-14">
+  <img src="images/imagenes4/rueda.png " class="float-left ">
+  <p class="text-yellow-300 uppercase  text-xl ml-28 "><b>PRESENTACIÓN Y DESARROLLO</b></p>
+  <p class="text-white ml-28">Set de archivos que contemplan formatos de uso</p>
+  <p class="text-white ml-28">libre y medidas para papelería, gigantografia y </p>
+  <p class="text-white ml-28">circulación digital.</p>
+  
+
+  </div>
+
+</div>
+<div class="flex justify-center">
+ <a href="#"><img src="images/imagenes4/botonConoceMas.png" class="mt-16" alt=""></a>
+</div> 
+      </article>
+    </div>
+  </section>
+  <!--Section APPLICA Y DESARROLLA-->
+  <section class="imagen5">
+    <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 ">
+      <article>
+        <div class="text-center mt-32">
+          <h1 class="text-yellow-300 text-5xl"><b>APPLICA</b></h1>
+          <h1 class="text-5xl"><b>& DESARROLLA TU IDEA</b></h1>
+        </div>
+        <div class="flex flex-row-reverse ">
+          <img src="images/imagenes5/telefono.png" class=" mr-56 mt-32" alt="">
+          <div class="mt-96 invisible">
+            <img src="imagenes2/botonConoce.png" class="" alt="">
+          </div>
+          <div class="mt-32 mr-28">
+            <h3 class="text-2xl"><b>Reunion técnica, creación de Briefing</b></h1>
+              <h1 class="text-2xl"><b>Acuerdos y redaccion de protocolos</b></h1>
+              <h1 class="text-2xl"><b>y contrato</b></h1>
+              <p class="text-xl">Todo el proyecto sera evaluado segun</p>
+              <p class="text-xl">corresponda y con un maximo de 3</p>
+              <p class="text-xl">modificaciones sustanciales.</p>
+              <p class="text-xl">Solo posterior a cancelacion de saldo,</p>
+              <p class="text-xl">se procedera a la subida en plataformas a fin.</p>
+              <div class="grid grid-cols-2 gap-2">
+                <img class="flex" src="images/imagenes5/botonGoogle.png" alt="">
+                <img class="flex" src="images/imagenes5/botonMac.png" alt="">
+              </div>
+          </div>
+        </div>
+
+      </article>
+  </section>
+
+
+  <!--section6 TIPOS DE LOGO-->
+  <section class="imagen6 ">
+    <div class=" max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 h py-20 px-3 ">
+      <article>
+
+
+
+
+        <div class="grid grid-cols-3">
+
+          <img class="float-left ml-10" src="images/imagenes6/logotipo.png">
+          <p class="text-yellow-400 text-3xl text-center mt-32 "><B>TIPOS DE <br> LOGO </B></p>
+          <img src="images/imagenes6/isologo.png" class="float-right ">
+        </div>
+    </div>
+
+    <div class="grid grid-cols-2 gap-96">
+      <img class="ml-36 mt-10 " src="images/imagenes6/isotipo.png">
+      <img class="mt-9 -ml-18" src="images/imagenes6/imagotipo.png">
+    </div>
+    <div class="flex text-center mt-20 ml-96 -mt">
+      <h1 class="text-white text-center text-2xl"><b>PSICOLOGIA, BRANDING & MAS...</b></h1>
+      <a href="#"><img src="images/imagenes6/boton.png" class="ml-10 -mt-4"> </a>
+    </div>
+    </article>
+  </section>
+
+  <!--section7 LOGOS--->
+  <section>
+    <div class="imagen7 max-w-71 mx-auto sm:px-6 lg:px-8 w-99 py-10">
+      <article>
+        <div class="grid grid-cols-4 mt gap-12 opacity-20">
+          <img src="images/imagenes7/transportes.png " class="mb-19" alt="">
+          <img src="images/imagenes7/MP.png" alt="">
+          <img src="images/imagenes7/puestoViejo.png" alt="">
+          <img src="images/imagenes7/banda.png" alt="">
+        <br>
+        </div>
+        <div class="grid grid-cols-5 gap-32 opacity-20">
+      <img src="images/imagenes7/pichecito.png" alt="">
+      <img src="images/imagenes7/autoFull.png" alt="">
+      <img src="images/imagenes7/kawell.png" alt="">
+      <img src="images/imagenes7/ropa.png" alt="">
+      <img src="images/imagenes7/macovik.png" alt="">
+        </div>
+      </article>
+  </section>
+
+
+  <!--footer-->
+  <footer class="imagen8">
+    <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 w-99 h">
+      <div class="grid grid-cols-3 gap-28 ">
+        <div>
+          <!--DIV ESCANEA NUESTRAS REDES-->
+          <h1 class="text-white text-2xl"><b>ESCANEA NUESTRAS REDES</b></h1>
+          <IMG src="images/imagenes8/hr1.png " class="mt-10"></IMG>
+
+          <IMG src="images/imagenes8/qrwsp.png" class="mt-10"></IMG>
+          <div class="flex">
+            <img src="images/imagenes8/fb.png" class="mt-10" alt="">
+            <img src="images/imagenes8/ig.png" class="mt-10 ml-10 " alt="">
+          </div>
+        </div>
+        <!--FIN DIV ESCANEA NUESTRAS REDES-->
+        <!--DIV COLABORADORES-->
+        <div>
+          <h1 class="text-white text-2xl"><b>COLABORADORES</b></h1>
+          <img src="images/imagenes8/hr2.png" class="mt-10" alt="">
+          <!--FIN DIV COLABORADORES-->
+        </div>
+        <!--DIV COLABORADORES-->
+        <div>
+          <h1 class="text-white text-2xl"><b>CONTACTO</b></h1>
+          <img src="images/imagenes8/hr3.png" class="mt-10" alt="">
+          <div class="">
+            <img src="images/imagenes8/data.png" class="float-left" alt="">
+            <p class="text-white  ">Coyhaique, Región de Aysen Patagonia Chilena </p>
+            <p class="text-white mt-10 ">+569 78809421</p>
+            <p class="text-white mt-5">portalcoyhaique@gmail.com </p>
+            <p class="text-white mt-5">www.portalcoyhaique.com </p>
+          </div>
+
+
+        </div>
+
+      </div>
+      <!--div columna-->
+  </footer>
+
+
+</body>
+
+</html>
